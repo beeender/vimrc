345bbb54a8fc4f3ed827aafbc03b63fd45f0b5dc
beeender <chenmulong@gmail.com>
2014-03-29 00:32:43 +0000

    Typo fix.

--- install.sh
+++ install.sh
@@ -2,6 +2,6 @@
 
 ln -s ~/.vim/vimrc ~/.vimrc
 ln -s ~/.vim/gvimrc ~/.gvimrc
-;Ignore doc/tags for bundles.
+#Ignore doc/tags for bundles.
 git config --global core.excludesfile '~/.gitexcludes'
 echo tags > ~/.gitexcludes
